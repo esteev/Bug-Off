7845489ee85bf81e8aa034361fd55bbcb1528d5e
Unknown <suryank@live.com>
2018-01-21 19:07:03 +0530

    First Commit

---
+++ Bug Off/Assets/Scripts/Region.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ConLand
+{
+    Lan, Fibre,
+}
+
+public enum ConAir
+{
+    Bluetooth, WLan, Radio
+}
+
+public enum ConSpace
+{
+    Sat
+}
+
+public class Region : MonoBehaviour {
+
+    public float[] connectivity= new float[3];     
+    
+    public float techLevel;
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Bug Off/Assets/Scripts/Ware.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ware : MonoBehaviour {
+
+    public float transmitivity;
+    public float severity; //lethality
+    public float visibility; //
+    
+
+    private float baseTransmitivity;
+    private float baseSeverity;
+    private float baseVisibilty;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
